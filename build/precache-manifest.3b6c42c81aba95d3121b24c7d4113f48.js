966e3a1e90723189eb136db2ba2ac57ac51dce4e
Asadbek-Azimjonov <asadbekazimjonov064@gmail.com>
2022-08-01 18:18:47 +0500

    changed
    changed

---
+++ build/precache-manifest.3b6c42c81aba95d3121b24c7d4113f48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1af87a7a1316152458f5978fabfd42f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fac418bec5bd6081600e",
+    "url": "/static/css/main.362bc119.chunk.css"
+  },
+  {
+    "revision": "5a90fff91e77ab09f0d8",
+    "url": "/static/js/2.660ddd80.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.660ddd80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac418bec5bd6081600e",
+    "url": "/static/js/main.5587ff1b.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  }
+]);
